891513775a0f41f0da3448a062f92a686e4fb277
ellas <ella.yap26@ycdsbk12.ca>
2024-04-22 12:45:43 -0400

    Updated Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,49 +1,89 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
+/**
+ * The program Sketch.java implements an image background, animates an image, and animates a shape.
+ * Additionally, the program incorporates basic edge collision detection
+ * such that the animated objects do not permenantaly move off the screen.
+ * The animated image moves in a non-linear path (circular).
+ * @author: E. Yap
+ */
 public class Sketch extends PApplet {
 
-  PImage imgMeteor;
+  PImage imgVolleyball;
+  PImage imgBackgroundNet;
 
-  // x and y coordinate for meteor
-  float fltMeteorX = 0;
-  float fltMeteorY = 0;
+  // x and y coordinate for volleyball
+  float fltVolleyballX = 150;
+  float fltVolleyballY = 50;
 
-  // meteor speed variables
-  float fltXSpeed = 1;
-  float fltYSpeed = 1;
+  // radius, angle, and speed variables
+  float fltVolleyballRadius;
+  float fltVolleyballAngle;
+  float fltVolleyballSpeed = (float) 0.05;
 
-  // define colours
-  int black = color(0);
+  // x and y coordinates for background volleyball net
+  float fltBackgroundX = 0;
+  float fltBackgroundY = 0;
 
+  // circle shape variables
+  float fltCircleX = 200;
+  float fltCircleY = 200;
+  float fltCircleSpeed = 3;
+
+  /**
+   * Size call
+   */
   public void settings() {
     size(400, 400);
   }
 
+  /**
+   * The initial set up values.
+   * The background and image.
+   */
   public void setup() {
+    // load volleyball
+    imgVolleyball = loadImage("imageVolleyball.png");
 
-    // load meteor
-    imgMeteor = loadImage("imageVolleyball.png");
+    // resize volleyball
+    imgVolleyball.resize(50,50);
 
-    // resize meteor
-    imgMeteor.resize(imgMeteor.width/8, imgMeteor.height/8);
+    // load background volleyball net
+    imgBackgroundNet = loadImage("imageBackgroundNet.png");
+
+    // resize background volleyball net
+    imgBackgroundNet.resize(400,400);
   }
 
+  /**
+   * Everything drawn to the screen.
+   * The background, image, and ellipse.
+   */
   public void draw() {
-    background(black);
+    // draw background volleyball net
+    image (imgBackgroundNet, fltBackgroundX, fltBackgroundY);
 
-    // draw meteor and move
-    image(imgMeteor, fltMeteorX, fltMeteorY);
+    // draw volleyball
+    image(imgVolleyball, fltVolleyballX, fltVolleyballY);
 
-    fltMeteorX += fltXSpeed;
-    fltMeteorY += fltYSpeed;
+    // colour and draw ellipse
+    fill(255);
+    ellipse (fltCircleX, fltCircleY, 50, 50);
 
-    if (fltMeteorX > width - imgMeteor.width || fltMeteorX < 0) {
-      fltXSpeed *= -1;
+    // basic edge collision detection such that the ellipse does not permenantly move off the screen
+    fltCircleY += fltCircleSpeed;
+    if (fltCircleY <= 25 || fltCircleY >= height - 50) {
+      fltCircleSpeed *= -1;
     }
 
-    if (fltMeteorY > height - imgMeteor.height || fltMeteorY < 0) {
-      fltYSpeed *= -1;
-    }
+    // move volleyball in a non-linear path (a circle)
+    fltVolleyballX += width / 3 * cos (fltVolleyballAngle) * fltVolleyballSpeed;
+    fltVolleyballY += height / 3 * sin (fltVolleyballAngle) * fltVolleyballSpeed;
+
+     // basic edge collision detection such that the volleyball does not permenantly move off the screen
+    fltVolleyballX = constrain (fltVolleyballX, 0, width - 50);
+    fltVolleyballY = constrain (fltVolleyballY, 0, height - 50);
+    fltVolleyballAngle += fltVolleyballSpeed;
   }
 }
